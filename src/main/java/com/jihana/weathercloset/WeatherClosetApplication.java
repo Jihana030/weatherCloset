12002b72abd8724af9da69b8cc5a5295d683967a
Jihana030 <wldnjs7618@naver.com>
2025-01-21 09:57:19 +0900

    project make

---
+++ src/main/java/com/jihana/weathercloset/WeatherClosetApplication.java
@@ -0,0 +1,13 @@
+package com.jihana.weathercloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherClosetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherClosetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihana/weathercloset/WeatherClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihana.weathercloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherClosetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
