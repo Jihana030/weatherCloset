8b05e4ca2821e1aa6bb6a03c13d1ba2775110c24
Jihana030 <wldnjs7618@naver.com>
2025-02-11 18:01:06 +0900

    api url setting2

--- src/main/frontend/src/components/Weather.jsx
+++ src/main/frontend/src/components/Weather.jsx
@@ -1,38 +1,81 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import useGeoLocation from "./hooks/useGeoLocation.jsx";
 
 function Weather() {
     // 오늘 날짜
     const now = new Date();
     const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const day = now.getDate().toString().padStart(2, "0");
     const week = ['일', '월', '화', '수', '목', '금', '토'];
     let weekday = week[now.getDay()];
 
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    // 30분 기준으로 값 변경
+    const nowHour = now.getHours();
+    let hour;
+    if(5 > nowHour >= 2){
+        hour = '02';
+    } else if (8 > nowHour >= 5){
+        hour = '05';
+    } else if (11 > nowHour >= 8){
+        hour = '08';
+    } else if (14 > nowHour >= 11){
+        hour = '11';
+    } else if (17 > nowHour >= 14){
+        hour = '14';
+    } else if (20 > nowHour >= 17){
+        hour = '17';
+    } else if (23 > nowHour >= 20){
+        hour = '20';
+    } else {
+        hour = '23';
+    }
+    const nowMinute = now.getMinutes();
+    let minute;
+    if (nowMinute >= 10) {
+        minute = '10';
+    } else {
+        minute = '00';
+    }
 
-    // 현재 위치(좌표값)
-    const geolocationOptions = {
-        enableHighAccuracy: true,
-        timeout: 1000 * 60, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
-        maximumAge: 1000 * 3600 * 24, // 24 hour
-    };
-    const {location: currentLocation, error: currentError} = useGeoLocation(geolocationOptions);
-    // location.latitude, location.longitude
+    // 현재 위치(좌표값)(위도(y) latitude, 경도(x) longitude)
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+    window.addEventListener('load', e => {
+        if (window.navigator.geolocation) {
+            window.navigator.geolocation.getCurrentPosition(success, error)
+        }
+    })
+    function success(e) {
+        setLatitude(Math.floor(e.coords.latitude))
+        setLongitude(Math.floor(e.coords.longitude))
+    }
+    function error(e) {}
+
+    // 위도, 경도로 현재 위치 찾기(kakao api)
+
 
     // 날씨 api
     const key = import.meta.env.VITE_WEATHER_API_KEY; //vite는 process 아닌 import.meta 사용
-    const endPoint = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst';
-    let url = `${endPoint}?serviceKey=${key}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}`;
-
+    const endPoint = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst';
+    let url = `${endPoint}?serviceKey=${key}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}${minute}&nx=${longitude}&ny=${latitude}`;
+    const [degrees, setDegrees] = useState('');
+    const [windSpeed, setWindSpeed] = useState('');
+    fetch(url)
+        .then(res => res.text())
+        .then(str=>new DOMParser().parseFromString(str,'application/xml'))
+        .then(xml=>{
+            const itemsTag = xml.getElementsByTagName('items');
+            const items = itemsTag[0];
+            // items.forEach(item=>{
+            //     if(item.getElementsByTagName('category').innerText ){}
+            // })
+            console.log(xml)
+        //     category WSD(풍속), TMP(1시간 기온), SKY(하늘상태), 값이
+        })
 
     return (
         <WeatherStyle>
-            <div className="date">{year}년 {month}월 {day}일 {weekday}요일</div>
+            <div className="date">{year}년 {month}월 {day}일 {weekday}요일 {hour}시</div>
             <div className="weather_data">
                 <div className="weather_area">대구</div>
                 <div className="weather_figure">
